46b5aa82fa0f998b51687f7c3fa2543d8c9cf358
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-06-29 22:30:16 +0530

    Update cal_aggregation.py

--- tests/cal_aggregation.py
+++ tests/cal_aggregation.py
@@ -2,8 +2,8 @@ import os
 import json
 import sys
 
-print(sys.argv[1])
 final_data = {}
+final_error = {}
 for file in os.listdir("./speedtest-result/"):
     try:
         if file.endswith(".json"):
@@ -14,17 +14,26 @@ for file in os.listdir("./speedtest-result/"):
             if not(inter in final_data):
                 final_data[inter] = {}
             f = open(json_path, 'r')
+            if (os.stat(json_path).st_size < 200):
+                if not(inter in final_error):
+                    final_error[inter] = {}
+                final_error[inter][d_or_u] = "Did not work"
             data = json.load(f)
             final_data[inter][d_or_u] = data[d_or_u] / 1000000.0
             f.close()
     except:
         pass
 
+if len(final_error.keys()) > 0:
+    with open('./speedtest-result/final_error--' + sys.argv[1] + '.json', 'w') as f1:
+        json.dump(final_error, f1, sort_keys=True, indent=4)
+    sys.exit(0)
+
 combined_data = final_data['all']
 final_data.pop('all')
 accumulated_upload = 0.0
 accumulated_download = 0.0
-print(final_data)
+
 for keys in final_data:
     accumulated_upload += final_data[keys]['upload']
     accumulated_download += final_data[keys]['download']
