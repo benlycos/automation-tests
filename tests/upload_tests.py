d404d12b1617a178fbf25fba5a3d26313a942384
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-06-29 21:08:40 +0530

    Create upload_tests.py

---
+++ tests/upload_tests.py
@@ -0,0 +1,4 @@
+import requests
+import sys
+import json
+req = requests.put(sys.argv[4], data=json.dumps({"content": sys.argv[2], "message": sys.argv[3]}), headers={"Authorization": "token " + sys.argv[1]})
