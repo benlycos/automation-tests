3adf0f2412906594da824091b16c72433b95cd93
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-06-29 23:34:52 +0530

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,7 +4,6 @@ if [[ $1 == "--help" ]]
 then
         echo "To run tests, Here are the commands"
         echo "run_tests.sh ap : To test aggregation"
-        echo "run_tests.sh ut : To upload the test"
         exit 0
 fi
 
@@ -14,14 +13,7 @@ then
         exit 0
 fi
 
-if [[ $1 == "ut" ]]
-then
-        ./upload_tests.sh
-        exit 0
-fi
-
 
 echo "To run tests, Here are the commands"
 echo "run_tests.sh ap : To test aggregation"
-echo "run_tests.sh ut : To upload the test"
 exit 0
