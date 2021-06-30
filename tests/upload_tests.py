9d3e9223b7fa98a170e3d98173828fd4f80345be
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-06-30 13:32:54 +0530

    Update upload_tests.py

--- tests/upload_tests.py
+++ tests/upload_tests.py
@@ -2,3 +2,5 @@ import requests
 import sys
 import json
 req = requests.put(sys.argv[4], data=json.dumps({"content": sys.argv[2], "message": sys.argv[3]}), headers={"Authorization": "token " + sys.argv[1]})
+if (req.status_code != 201):
+    print("Error")
