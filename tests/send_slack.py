aa76befd8eaa2b1e376189f97ad731907c292ecb
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-01 14:32:56 +0530

    Update send_slack.py

--- tests/send_slack.py
+++ tests/send_slack.py
@@ -2,4 +2,5 @@ import requests
 import json
 import sys
 payload = {"text": sys.argv[1]}
-req = requests.post("https://hooks.slack.com/services/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", data=json.dumps(payload))
+req = requests.post(sys.argv[2], data=json.dumps(payload))
+print(req.text)
