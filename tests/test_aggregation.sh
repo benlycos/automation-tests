9798c5fdd8f129ef601f9e044c042a2b771ea5e2
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-01 14:41:59 +0530

    Update test_aggregation.sh

--- tests/test_aggregation.sh
+++ tests/test_aggregation.sh
@@ -10,6 +10,7 @@ mv ./speedtest-result/*.json ./speedtest-result/old_test/
 mv ./speedtest-result/*.txt ./speedtest-result/old_test/
 SLACK_URL=$(echo $(cat ./slack_url.gpg) | openssl aes-256-cbc -d -a -pass pass:somepassword)
 SERVER_IP=$(curl -s checkip.amazonaws.com)
+python send_slack.py "Testing starting on box ${SRL_NO} at $(date)" "${SLACK_URL}"
 ./test_speed.sh upload ${NUM}x $RAN_STR
 route -n > ./speedtest-result/all--upload--$RAN_STR.log
 python speedtest.py --no-download --json > ./speedtest-result/all--upload--$RAN_STR.json
@@ -24,3 +25,4 @@ sleep 5
 
 python cal_aggregation.py $RAN_STR
 ./upload_tests.sh ${RAN_STR}
+python send_slack.py "Test ended and results uploaded for ${SRL_NO} at $(date)" "${SLACK_URL}"
