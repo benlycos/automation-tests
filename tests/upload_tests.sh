d982321ad8b90889ada3d9ecaf042642fda87f44
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-04 12:52:33 +0530

    Update upload_tests.sh

--- tests/upload_tests.sh
+++ tests/upload_tests.sh
@@ -16,4 +16,4 @@ python upload_tests.py "${TOK}" "${CONTENT}" "${MSGG}" "${URL}"
 echo "https://github.com/benlycos/automation-results/blob/main/${DTE}/${SRL_NO}/${FLD}.tar.gz" > ./speedtest-result/uploaded_link--$1.txt
 rm -rf ${FLD}.tar.gz
 SLACK_URL=$(cat ./slack_url.gpg | openssl aes-256-cbc -d -a -pass pass:somepassword)
-python send_slack.py "Test ended and results uploaded at https://github.com/benlycos/automation-results/blob/main/${DTE}/${SRL_NO}/${FLD}.tar.gz for ${SRL_NO} at $(date)" "${SLACK_URL}"
+python send_slack.py "Test ended and results uploaded. Report available at https://benlycos.github.io/automation-results/report/home?file-path=${DTE}/${SRL_NO}/${FLD}.tar.gz for ${SRL_NO} at $(date)" "${SLACK_URL}"
