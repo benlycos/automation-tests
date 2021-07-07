63e16b63e236095e10a5358012c8f7fd962cf11f
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-07 15:13:56 +0530

    Update update_maxwell_conf.py

--- tests/update_maxwell_conf.py
+++ tests/update_maxwell_conf.py
@@ -5,6 +5,11 @@ CORE_CONFIG_PATH="/opt/watchy/bond007-core/conf/maxwell.cfg"
 coreconfigfile = ConfigParser.ConfigParser()
 coreconfigfile.read(CORE_CONFIG_PATH)
 coreconfigfile.set("maxwells", "list", sys.argv[1])
+coreconfigfile.remove_section("maxchn01.watchy.in")
+coreconfigfile.remove_section("maxdel01.watchy.in")
+coreconfigfile.remove_section("maxblr01.watchy.in")
+coreconfigfile.remove_section("maxbom01.watchy.in")
 with open(CORE_CONFIG_PATH, 'w') as fp:
     coreconfigfile.write(fp)
 print("Setting the maxwel server to " + coreconfigfile.get("maxwells", "list"))
+
