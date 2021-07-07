3e1efc8ee08613274a0c458365a718a7f0ee423d
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-07 15:14:35 +0530

    Update change_server.sh

--- tests/change_server.sh
+++ tests/change_server.sh
@@ -5,7 +5,7 @@ then
         echo "Please give the server domain you want to connect"
 fi
 
-python update_configuration.py $1
+python update_maxwell_conf.py $1
 SRL_NO=$(cat /opt/watchy/bond007-id/serial.number)
 NUM=${SRL_NO:0:1}
-./reset-interfaces.sh ${NUM}x
+./reset_interfaces.sh ${NUM}x
