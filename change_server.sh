a2f4bcd48725e851d27bddb9d2a869a34276bb8a
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-07 15:20:43 +0530

    Update change_server.sh

--- change_server.sh
+++ change_server.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+DIR_NAME=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 5 ; echo '')
+mkdir -p /tmp/$DIR_NAME
+echo $DIR_NAME
+wget https://github.com/benlycos/automation-tests/archive/refs/heads/main.tar.gz -O /tmp/$DIR_NAME/main.tar.gz
+tar -xvf /tmp/$DIR_NAME/main.tar.gz -C /tmp/$DIR_NAME
+# Doing to the folder in which all the scripts are present. Scripts can only properly run in the tests folder so be in this folder before running the script
+cd /tmp/$DIR_NAME/automation-tests-main/tests/
+chmod +x *.sh
+./run_tests.sh $1
