52a160e108e7f97e7e3c00f57dda7c58524630c2
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-07 07:33:58 +0530

    Create manage-hosts.sh

---
+++ tests/manage-hosts.sh
@@ -0,0 +1,43 @@
+
+#!/bin/sh
+
+# run:
+# sudo ./manage-etc-hosts.sh add test.com
+# sudo ./manage-etc-hosts.sh remove 10.20.1.2 test.com
+
+# PATH TO YOUR HOSTS FILE
+ETC_HOSTS=/etc/hosts
+
+remove() {
+    HOSTNAME=$1
+    if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+    then
+        echo "$HOSTNAME Found in your $ETC_HOSTS, Removing now...";
+        sudo sed -i".bak" "/$HOSTNAME/d" $ETC_HOSTS
+    else
+        echo "$HOSTNAME was not found in your $ETC_HOSTS";
+    fi
+}
+
+add() {
+    IP=$1
+    HOSTNAME=$2
+    HOSTS_LINE="$IP[[:space:]]$HOSTNAME"
+    line_content=$( printf "%s\t%s\n" "$IP" "$HOSTNAME" )
+    if [ -n "$(grep -P $HOSTS_LINE $ETC_HOSTS)" ]
+        then
+            echo "$line_content already exists : $(grep $HOSTNAME $ETC_HOSTS)"
+        else
+            echo "Adding $line_content to your $ETC_HOSTS";
+            sudo -- sh -c -e "echo '$line_content' >> /etc/hosts";
+
+            if [ -n "$(grep -P $HOSTNAME $ETC_HOSTS)" ]
+                then
+                    echo "$line_content was added succesfully";
+                else
+                    echo "Failed to Add $line_content, Try again!";
+            fi
+    fi
+}
+
+$@
