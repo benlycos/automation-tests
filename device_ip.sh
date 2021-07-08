95933485b06fbf5d0d4fc0a035f684ae64ef8ec7
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-08 14:52:02 +0530

    Update device_ip.sh

--- device_ip.sh
+++ device_ip.sh
@@ -1,2 +1,2 @@
-P_IP=$(cat /etc/openvpn/servers/pa-1194/logs/openvpn-status.log | grep $1 | tail -n 1 | sed -e 's/,.*/\ /g')
+P_IP=$(sudo cat /etc/openvpn/servers/pa-1194/logs/openvpn-status.log | grep $1 | tail -n 1 | sed -e 's/,.*/\ /g')
 echo $P_IP
