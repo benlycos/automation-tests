2e7c6b7519af53dc3f16089aaee482a2fd052f38
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-08 16:34:32 +0530

    Update device_ip.sh

--- device_ip.sh
+++ device_ip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-P_IP=$(sudo cat /etc/openvpn/servers/pa-1194/logs/openvpn-status.log | grep $1 | tail -n 1 | sed -e 's/,.*/\ /g')
+P_IP=$(cat /etc/openvpn/servers/pa-1194/logs/openvpn-status.log | grep $1 | tail -n 1 | sed -e 's/,.*/\ /g')
 if [ "${P_IP}" = "" ]
 then 
         echo "NOTHING"
