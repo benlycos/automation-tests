1db3b10e800c3cde05ecb87b519158be8c44265c
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-08 15:17:55 +0530

    Update device_ip.sh

--- device_ip.sh
+++ device_ip.sh
@@ -1,2 +1,5 @@
 P_IP=$(sudo cat /etc/openvpn/servers/pa-1194/logs/openvpn-status.log | grep $1 | tail -n 1 | sed -e 's/,.*/\ /g')
-echo $P_IP
+while ! ping -c1 $P_IP &>/dev/null
+        do echo "Ping Fail - `date`"
+done
+echo "Host Found - `date`"
