3b27e886550a1e4d9e8749edcf10f6dcc0b8daa8
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-09 23:22:26 +0530

    Update change_server.sh

--- tests/change_server.sh
+++ tests/change_server.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+SRL_NO=$(cat /opt/watchy/bond007-id/serial.number)
 SLACK_URL=$(cat ./slack_url.gpg | openssl aes-256-cbc -d -a -pass pass:somepassword)
 if [[ $1 == "" ]]
 then
-        curl -X POST --data-urlencode "payload={\"text\": \"Change server for ${SLK_KEY} didnt happen. Server that can be selected are bom01,chn01,blr01,del01 `date` \"}" ${SLACK_URL}
+        curl -X POST --data-urlencode "payload={\"text\": \"Change server for ${SRL_NO} didnt happen. Server that can be selected are bom01,chn01,blr01,del01 `date` \"}" ${SLACK_URL}
         echo "Please give the server domain you want to connect"
         exit 0
 fi
 
+curl --output /dev/null --silent --head --fail -X POST --data-urlencode "payload={\"text\": \"Going to change the server for ${SRL_NO} to ${1}.`date` \"}" ${SLACK_URL}
+
 python update_maxwell_conf.py $1
-SRL_NO=$(cat /opt/watchy/bond007-id/serial.number)
 NUM=${SRL_NO:0:1}
 ./reset_interface.sh ${NUM}x
+
+until $(curl --output /dev/null --silent --head --fail -X POST --data-urlencode "payload={\"text\": \"Change server for ${SRL_NO} has happened successfully to ${1}.`date` \"}" ${SLACK_URL}); do
+    printf '.'
+    sleep 5
+done
