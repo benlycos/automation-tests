faca6800a8a14f8556b91602015f37eed142e7c8
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-09 23:10:36 +0530

    Update change_server.sh

--- tests/change_server.sh
+++ tests/change_server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+SLACK_URL=$(cat ./slack_url.gpg | openssl aes-256-cbc -d -a -pass pass:somepassword)
 if [[ $1 == "" ]]
 then
+        curl -X POST --data-urlencode "payload={\"text\": \"Change server for ${SLK_KEY} didnt happen. Server that can be selected are bom01,chn01,blr01,del01 `date` \"}" ${SLACK_URL}
         echo "Please give the server domain you want to connect"
+        exit 0
 fi
 
 python update_maxwell_conf.py $1
