92504cc27409f50ec717fa96ba8c9904c02b1a66
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-26 19:03:33 +0530

    Update device_ip.sh

--- device_ip.sh
+++ device_ip.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-P_IP=$(cat /etc/openvpn/servers/pa-1194/logs/openvpn-status.log | grep $1 | tail -n 1 | sed -e 's/,.*/\ /g')
-if [ "${P_IP}" = "" ]
-then 
-        echo "NOTHING"
-        exit 0
-else 
-        ping -c1 $P_IP > /dev/null
-        if [ $? -eq 0 ]
-        then 
-                echo $P_IP
+
+ALL_P_IP=$(cat /etc/openvpn/servers/pa-1194/logs/openvpn-status.log | grep $1 | grep -v "^pa" | cut -d"," -f1)
+for  P_IP in $ALL_P_IP
+do
+
+        if [ "${P_IP}" = "" ]
+        then
+                echo "NOTHING"
+                exit 0
+        else
+                nc -vz -w 1 $P_IP 22 &> /dev/null
+                if [ $? -eq 0 ]
+                then
+                        echo $P_IP
+                        exit 0
+                fi
+                echo "NOTHING"
                 exit 0
         fi
-        echo "NOTHING"
-        exit 0
-fi
+done
