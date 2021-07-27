1ed44092293cd048f731e676b944a59c9fd0b681
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-27 09:42:45 +0530

    Update run-script-remote.sh

--- run-script-remote.sh
+++ run-script-remote.sh
@@ -3,4 +3,4 @@ BOX_KEY=$1
 TMP_DIR=$(mktemp -d -t watchy-XXXXXXXXXX)
 TOK=$(wget -q https://raw.githubusercontent.com/benlycos/automation-tests/main/tests/key.gpg -O - | openssl aes-256-cbc -d -a -pass pass:somepassword)
 curl -s --header "Authorization: token $TOK" --header 'Accept: application/vnd.github.v3.raw'  -o "${TMP_DIR}/$1" --location "https://api.github.com/repos/benlycos/box-keys/contents/$1"
-ssh -q -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no  -i $TMP_DIR/$1 watchy@$2 "nohup \"${3}\" > test.out 2> test.err < /dev/null &"
+ssh -q -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no  -i $TMP_DIR/$1 watchy@$2 "$3"
