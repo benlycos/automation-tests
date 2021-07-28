1efea1cdc8fc54ef9d9394bafd9ad2599c15ff73
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-28 21:42:39 +0530

    Update run_aggregation_percentage.sh

--- run_aggregation_percentage.sh
+++ run_aggregation_percentage.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 TMP_DIR=$(mktemp -d -t watchy-XXXXXXXXXX)
 wget -bqc -O ${TMP_DIR}/main.tar.gz https://github.com/benlycos/automation-tests/archive/refs/heads/main.tar.gz > ${TMP_DIR}/all.log
-PID=$(echo $!)
+sleep 1
+PID=$(cat all.log | grep "pid" | tr -d -c 0-9)
 echo $PID >> ${TMP_DIR}/all.log
 timeout 20 tail --pid=$PID -f /dev/null
 tar -xvf ${TMP_DIR}/main.tar.gz -C ${TMP_DIR} >> ${TMP_DIR}/all.log
