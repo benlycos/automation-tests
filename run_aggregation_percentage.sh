7916a37e70921b60ef19613c4e90819ba009c43e
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-28 20:48:05 +0530

    Update run_aggregation_percentage.sh

--- run_aggregation_percentage.sh
+++ run_aggregation_percentage.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 TMP_DIR=$(mktemp -d -t watchy-XXXXXXXXXX)
 wget -bqc -O ${TMP_DIR}/main.tar.gz https://github.com/benlycos/automation-tests/archive/refs/heads/main.tar.gz > ${TMP_DIR}/wget.log
+PID=$(echo $!)
+timeout 20 tail --pid=$PID -f /dev/null
 tar -xvf ${TMP_DIR}/main.tar.gz -C ${TMP_DIR}
 # Doing to the folder in which all the scripts are present. Scripts can only properly run in the tests folder so be in this folder before running the script
 cd ${TMP_DIR}/automation-tests-main/tests/
