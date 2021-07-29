724730286624df8d0c8ae82d3220b6e658e4db96
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-29 14:14:18 +0530

    Update change_server.sh

--- change_server.sh
+++ change_server.sh
@@ -30,4 +30,4 @@ tar -xvf ${TMP_DIR}/main.tar.gz -C ${TMP_DIR}
 # Doing to the folder in which all the scripts are present. Scripts can only properly run in the tests folder so be in this folder before running the script
 cd ${TMP_DIR}/automation-tests-main/tests/
 chmod +x *.sh
-./change_server.sh $SERVER_NAME 
+nohup ./change_server.sh $SERVER_NAME &
